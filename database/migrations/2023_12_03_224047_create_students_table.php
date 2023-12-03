be4054409946ec1635fa2d48531627ae35c91752
Funmi4194 <funmilayoolayiwola03@gmail.com>
2023-12-03 23:44:14 +0100

    initial commit

---
+++ database/migrations/2023_12_03_224047_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('level_id');
+            $table->string('matric_no')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
