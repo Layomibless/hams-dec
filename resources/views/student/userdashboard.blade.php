b01319ba1c88590efbef33d2631b9b2d76f059fe
Funmi4194 <funmilayoolayiwola03@gmail.com>
2023-12-25 11:04:51 +0100

    initial commit

--- resources/views/student/userdashboard.blade.php
+++ resources/views/student/userdashboard.blade.php
@@ -4,7 +4,7 @@
     <!-- row -->	
     <div class="page-titles">
         <ol class="breadcrumb">
-            <li><h5 class="bc-title">User Profile</h5></li>
+            <li><h5 class="bc-title">User Dashboard</h5></li>
             <li class="breadcrumb-item"><a href="javascript:void(0)">
                 <svg width="17" height="17" viewbox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2.125 6.375L8.5 1.41667L14.875 6.375V14.1667C14.875 14.5424 14.7257 14.9027 14.4601 15.1684C14.1944 15.4341 13.8341 15.5833 13.4583 15.5833H3.54167C3.16594 15.5833 2.80561 15.4341 2.53993 15.1684C2.27426 14.9027 2.125 14.5424 2.125 14.1667V6.375Z" stroke="#2C2C2C" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -12,143 +12,32 @@
                 </svg>
                 Home </a>
             </li>
-            <li class="breadcrumb-item active"><a href="javascript:void(0)">Manage Client</a></li>
+            <li class="breadcrumb-item active"><a href="javascript:void(0)">User Dashboard</a></li>
         </ol>
         <a class="text-primary fs-13" data-bs-toggle="offcanvas" href="#offcanvasExample1" role="button" aria-controls="offcanvasExample1">+ Add Task</a>
     </div>
 <!-- row -->	
 <div class="container-fluid">
 <div class="row">
-    <div class="col-xl-4  col-lg-6">
+    <div class="col-xl-4 col-lg-12 col-xxl-4 col-sm-12">
         <div class="card">
-            <div class="card-header  border-0 pb-0">
-                <h4 class="card-title">Notifications</h4>
-            </div>
-            <div class="card-body p-0"> 
-                <div id="DZ_W_Todo1" class="widget-media dz-scroll height370 my-4 px-4">
-                    <ul class="timeline">
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2">
-                                    <img alt="image" width="50" src="images/avatar/1.jpg">
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Dr sultads Send you Photo</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-primary light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2 media-info">
-                                    KG
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Resport created successfully</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-info light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2 media-success">
-                                    <i class="fa fa-home"></i>
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Reminder : Treatment Time!</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-success light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                         <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2">
-                                    <img alt="image" width="50" src="images/avatar/1.jpg">
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Dr sultads Send you Photo</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-primary light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2 media-danger">
-                                    KG
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Resport created successfully</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-danger light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2 media-primary">
-                                    <i class="fa fa-home"></i>
-                                </div>
-                                <div class="media-body">
-                                    <h5 class="mb-1">Reminder : Treatment Time!</h5>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-primary light sharp" data-bs-toggle="dropdown">
-                                        <svg width="18px" height="18px" viewbox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="#">Edit</a>
-                                        <a class="dropdown-item" href="#">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+            <div class="card-body">
+                <h4 class="my-2">Please Read the Hall Regulations below to complete your accommodation application
+                    Hall Regulations</h4>
+                    <p>Only bona-fide (registered) students of the University are allowed to live in the Halls of Residence.
+                    Accommodation in the halls of residence is a privilege and not a right.
+                    Allocated accommodation is not transferable from one student to another.
+                    Squatting of anybody overnight is illegal; offenders will forfeit their accommodation.
+                    Sale of allocated accommodation in the Halls of Residence is prohibited.
+                    Students must not bring into students’ rooms heavy electrical gadgets like electric cookers/stoves, washing machines, drier, grills, immersion heaters, refrigerators etc.
+                    Any student who is found guilty of disturbing the peace of the hall shall be required to withdraw from the hall.
+                    Fighting is not allowed. Student who engages in fighting shall forfeit accommodation and face appropriate disciplinary measures.
+                    Both stale and new students should note that membership of cult group is prohibited. Severe disciplinary actions await such members.
+                    It is mandatory for students to clean their rooms.
+                    Students are required to return their keys and the other University property at the close of every semester/session.</p>
+                    <hr>
+                    
+                <a href="javascript:void();;" class="btn my-2 btn-primary btn-lg px-4">Apply Here</a>
             </div>
         </div>
     </div>
